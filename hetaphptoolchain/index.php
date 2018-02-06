fa8b99b5b1835cbf3e8715de4a4f46b39dc34b05
hetap1604 <patelheta083@gmail.com>
2018-02-06 08:15:22 +0530

    Add files via upload

---
+++ hetaphptoolchain/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Starter Application</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<table>
+		<tr>
+			<td style='width: 30%;'>
+				<img class = 'newappIcon' src='images/newapp-icon.png'>
+			</td>
+			<td>
+				<h1 id = "message"><?php echo "Hello Heta!!"; ?></h1>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
